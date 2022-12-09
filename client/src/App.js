5f53f7e98b0fd6173c3919976d8ad5ac8f0d02c9
Kyle Davis <kyle.davis@ucdenver.edu>
2022-12-09 08:05:59 -0700

    initial fontend set up

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { useEffect, useState } from 'react'
+import { CheckSession } from './services/Auth'
+const BASE_URL = 'http://localhost:3001/guide'
 
 function App() {
+  const [authenticated, toggleAuthenticated] = useState(false)
+  const [user, setUser] = useState(null)
+
+  // Logout function
+  const handleLogOut = () => {
+    setUser(null)
+    toggleAuthenticated(false)
+    localStorage.clear()
+  }
+
+  // Checks for user using token
+  const checkToken = async () => {
+    const user = await CheckSession()
+    setUser(user)
+    toggleAuthenticated(true)
+  }
+
+  useEffect(() => {
+    const token = localStorage.getItem('token')
+    if (token) {
+      checkToken()
+    }
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav></nav>
+      <main></main>
+      <footer></footer>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
