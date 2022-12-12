93587837caad534be2bfe23e067805e2bd712243
Kyle Davis <kyle.davis@ucdenver.edu>
2022-12-12 08:32:05 -0700

    working on checklist

--- client/src/components/ChecklistInfo.jsx
+++ client/src/components/ChecklistInfo.jsx
@@ -1,20 +1,42 @@
 import React from 'react'
 import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
 import axios from 'axios'
 
 const ChecklistInfo = (props) => {
+  let { user_Id } = useParams()
   const [checklist, setChecklist] = useState([])
+  const [isChecked, setIsChecked] = useState(false)
+  const [newChecklist, setNewChecklist] = useState({ userId: '', mountainId: '', hasClimbed: '' })
   const BASE_URL = props.BASE_URL
 
-  useEffect(() => {
-  const checklistCall = async () => {
-    let response = await axios.get(`${BASE_URL}/checklist/mountain`)
-    setChecklist(response.data)
+  const checkHandler = async (e) => {
+    setIsChecked(!isChecked)
+    if (!isChecked){
+      e.preventDefault()
+      const createChecklist = {
+        ...newChecklist,
+        userId: user_Id,
+        mountainId: props.id,
+        hasClimbed: 'True'
+      }
+      await axios.post(`${BASE_URL}/checklist/create`, createChecklist)
+    }
   }
-  checklistCall()
-}, [])
+console.log(user_Id)
+
   return (
+    <div>
     <div><h3>{props.name}</h3></div>
+    {props.users.map((climbed) => (
+        <div>
+          {climbed.hasClimbed === true ? <h4>Yes</h4> : <h4>No</h4>}
+        </div>
+      ))}
+    <input type="checkbox" id="checkbox" checked={isChecked} onChange={checkHandler} />
+    <label htmlFor="checkbox">I have climbed this mountain</label>
+      <p>The checkbox is {isChecked ? "checked" : "unchecked"}</p>
+    </div>
   )
 }
 
--- client/src/pages/Checklist.jsx
+++ client/src/pages/Checklist.jsx
@@ -5,14 +5,15 @@ import axios from 'axios'
 import ChecklistInfo from '../components/ChecklistInfo'
 
 const Checklist = (props) => {
-  let { userId } = useParams()
+  let { user_Id } = useParams()
   const [mountains, setMountains] = useState([])
   const BASE_URL = 'http://localhost:3001/guide'
 
   useEffect(() => {
     const mountainCall = async () => {
-      let response = await axios.get(`${BASE_URL}/mountain/`)
+      let response = await axios.get(`${BASE_URL}/mountain/cl`)
       setMountains(response.data)
+      console.log(response.data)
     }
     mountainCall()
   }, [])
@@ -20,8 +21,9 @@ const Checklist = (props) => {
   return  (
     <div>
       {mountains.map((mountain) => (
-        <ChecklistInfo key={mountain.id} id={mountain.id} name={mountain.name} image={mountain.image} BASE_URL={BASE_URL}/>
+        <ChecklistInfo key={mountain.id} id={mountain.id} name={mountain.name} image={mountain.image} BASE_URL={BASE_URL} users={mountain.mountain_cl}/>
       ))}
+      <button className='create-post-button'>Save Page</button>
     </div>
   ) 
 }
